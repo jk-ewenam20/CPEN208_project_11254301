508004edb154e494d7e3aae6d886a63ca46d2384
Jonathan Kwabena Ewenam Kudiabor <jonathan.ewenam16@gmail.com>
2024-07-20 00:05:31 +0000

    Added fetching registered courses

--- ses_sms_frontend/src/app/dashboard/register_courses/page.tsx
+++ ses_sms_frontend/src/app/dashboard/register_courses/page.tsx
@@ -1,57 +1,66 @@
-import React from "react";
+"use client";
 
-const RegisterCourses = () => {
-  return (
-    <div>
-      <div className="flex w-full flex-col">
-        <div className=" bg-base-300 rounded-box grid h-72 place-items-center">
-          content
-        </div>
-        <div className="divider"></div>
-        <p className="font-serif font-semibold p-7">Registered Courses</p>
-        <div className="bg-base-300 rounded-box grid place-items-stretch">
-          <div className="overflow-x-auto">
-            <table className="table table-zebra table-pin-rows">
-              {/* head */}
-              <thead>
-                <tr>
-                  <th></th>
-                  <th>Course code</th>
-                  <th>Course Title</th>
-                  <th>Credit Hours</th>
-                  <th>Lecturer</th>
-                  <th>Teaching Assistant</th>
-                </tr>
-              </thead>
-              <tbody>
-                {/* row 1 */}
-                <tr>
-                  <th>1</th>
-                  <td>Cy Ganderton</td>
-                  <td>Quality Control Specialist</td>
-                  <td>Blue</td>
-                </tr>
-                {/* row 2 */}
-                <tr>
-                  <th>2</th>
-                  <td>Hart Hagerty</td>
-                  <td>Desktop Support Technician</td>
-                  <td>Purple</td>
-                </tr>
-                {/* row 3 */}
-                <tr>
-                  <th>3</th>
-                  <td>Brice Swyre</td>
-                  <td>Tax Accountant</td>
-                  <td>Red</td>
-                </tr>
-              </tbody>
-            </table>
+import { useSession } from "next-auth/react";
+
+interface RegistrationInfo {
+  registration_id: number;
+  course_code: string;
+  course_title: string;
+  credits: number;
+}
+
+const RegisterCourses = async () => {
+  const { data: session, status } = useSession();
+
+  {
+    ("use server");
+  }
+  const response = await fetch(
+    `http://localhost:20201/api/st_reg_courses/${session?.user.id}`
+  );
+  const dataList: RegistrationInfo[] = await response.json();
+
+  if (status === "authenticated") {
+    return (
+      <div>
+        <div className="flex w-full flex-col">
+          <div className=" bg-base-300 rounded-box grid h-72 place-items-center">
+            content
+          </div>
+          <div className="divider"></div>
+          <p className="font-serif font-semibold p-7">Registered Courses</p>
+          <div className="bg-base-300 rounded-box grid place-items-stretch">
+            <div className="overflow-x-auto">
+              <table className="table table-zebra table-pin-rows">
+                {/* head */}
+                <thead>
+                  <tr>
+                    <th>Registration Id</th>
+                    <th>Course code</th>
+                    <th>Course Title</th>
+                    <th>Credit Hours</th>
+                    <th>Lecturer</th>
+                    <th>Teaching Assistant</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {/* row 1 */}
+                  {dataList.map((dataList) => (
+                    <tr key={dataList.registration_id}>
+                      <td>{dataList.registration_id}</td>
+                      <td> {dataList.course_code} </td>
+                      <td> {dataList.course_title} </td>
+                      <td> {dataList.credits} </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default RegisterCourses;
